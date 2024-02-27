e1f0bb2f3c041d6aab1a6030b23a908c63cacaa3
Khair <khairsmp1@gmail.com>
2024-02-27 09:34:01 +0700

    Tutorial 4 finished

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import show_main, create_book, show_xml, show_json, show_xml_by_id, show_json_by_id
-from main.views import register, login_user, logout_user
+from main.views import register, login_user, logout_user, edit_book, delete_book
 
 app_name = 'main'
 
@@ -14,4 +14,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-book/<int:id>', edit_book, name='edit_book'),
+    path('delete/<int:id>', delete_book, name='delete_book'),
 ]
